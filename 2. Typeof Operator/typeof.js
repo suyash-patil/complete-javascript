46769aab5fccc84e1f0d506f95b52685fbe56077
Suyash Patil <suyashpatil532001@gmail.com>
2022-07-14 23:43:44 +0530

    typeof operator studied

---
+++ 2. Typeof Operator/typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof 4); // number
+console.log(typeof 'hello') // string
+console.log(typeof 123456789n) // bigint
+console.log(typeof true) // boolean
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object: It is a well known error in JS
+console.log(typeof Math); // Math Object
