4e43ef0ab8c348dd548390c0e77d8551088a2f41
Suyash Patil <suyashpatil532001@gmail.com>
2022-07-14 23:25:22 +0530

    files separated for different types of data types

---
+++ DataTypes/string.js
@@ -0,0 +1,7 @@
+let str = 'hello';
+let str2 = "World"; // both single and double quotes are same
+let str3 = `123`; // used to embed variables into strings
+
+let age = 23;
+const message = `John is ${age} years old`;
+console.log(message);
