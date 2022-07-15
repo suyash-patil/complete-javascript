277f18716c32187ff020dd23d02b7a8ebcf137d8
Suyash Patil <suyashpatil532001@gmail.com>
2022-07-15 11:57:23 +0530

    logical Operators studied

---
+++ 4. Math Operations/logicaloperators.js
@@ -0,0 +1,16 @@
+/*
+There are 3 types of it:
+Logical OR (||)
+Logical AND (&&)
+Logical NOT (!)
+*/
+
+let firstCondition = true;
+let secondCondition = false;
+
+console.log(firstCondition || secondCondition); // true as one of the condition is true
+
+console.log(firstCondition && secondCondition); // false, as both needs to be true to get the result as true
+
+console.log(!firstCondition); // opposite of true is false
+
