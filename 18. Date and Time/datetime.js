2ccb7d34fc2020e3a0d63ec6e0fc86d4ddddf21e
Suyash Patil <suyashpatil532001@gmail.com>
2022-07-16 14:30:32 +0530

    date and time studied in JS

---
+++ 18. Date and Time/datetime.js
@@ -0,0 +1,9 @@
+// Date is also an object to store date and time in JS
+
+let date = new Date();
+
+console.log(date);
+
+// getDate, getDay, getMonth, getFullYear
+
+console.log(date.getFullYear(), date.getMonth(), date.getDay(), date.getDate());
